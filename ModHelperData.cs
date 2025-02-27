92456cf7b18c3a46a3c180d2c7cb713703e8dad9
ah987 <ah987@students.waikato.ac.nz>
2025-02-28 11:31:58 +1300

    updated version

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -3,7 +3,7 @@ namespace PlacementHelper;
 public static class ModHelperData
 {
     public const string WorksOnVersion = "47.0";
-    public const string Version = "0.0.4";
+    public const string Version = "0.0.5";
     public const string Name = "PlacementHelper";
 
     public const string Description = "A utility mod to nudge, snap, squeeze, and rotate tower placements.";
